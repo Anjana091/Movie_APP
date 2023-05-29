2624657fdf77ae3fb79928985ddca8f649f41944
Anjana091 <anjanasuresh091@gmail.com>
2023-05-29 13:15:12 +0530

    meta tag added

--- script.js
+++ script.js
@@ -58,7 +58,7 @@ function loadMovieDetails(){
             movieSearchBox.value = "";
             const result = await fetch(`http://www.omdbapi.com/?i=${movie.dataset.id}&apikey=50435948`);
             const movieDetails = await result.json();
-            // console.log(movieDetails);
+           console.log(movieDetails);
             displayMovieDetails(movieDetails);
         });
     });
